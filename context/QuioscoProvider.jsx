cec282e42a0c31fe66aa4510014f60d2cadef5d2
AlxMen <alejandrocs402@gmail.com>
2024-03-15 14:16:30 +0000

    vista usuario terminada

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,61 +1,106 @@
-import { useState, useEffect, createContext } from 'react'
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
-const QuioscoContext = createContext()
+const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
-  
-  const [categorias, setCategorias] = useState([])
+  const [categorias, setCategorias] = useState([]);
   const [categoriaActual, setCategoriaActual] = useState({});
-  const [producto, setProducto] = useState({})
-  const [modal, setModal] = useState(false)
-  const [pedido, setPedido] = useState([])
-  const [paso, setPaso] = useState(1)
+  const [producto, setProducto] = useState({});
+  const [modal, setModal] = useState(false);
+  const [pedido, setPedido] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [total, setTotal] = useState(0);
+
+  const router = useRouter();
 
   const obtenerCategorias = async () => {
-    const { data } = await axios('/api/categorias')
-    setCategorias(data)
-  }
+    const { data } = await axios("/api/categorias");
+    setCategorias(data);
+  };
   useEffect(() => {
-    obtenerCategorias()
-  }, [])
+    obtenerCategorias();
+  }, []);
 
   useEffect(() => {
     setCategoriaActual(categorias[0]);
   }, [categorias]);
 
+  useEffect(() => {
+    const nuevoTotal = pedido.reduce(
+      (total, producto) => producto.precio * producto.cantidad + total,
+      0
+    );
+
+    setTotal(nuevoTotal);
+  }, [pedido]);
 
   const handleClickCategoria = (id) => {
-    const categoria = categorias.filter(cat => cat.id === id)
+    const categoria = categorias.filter((cat) => cat.id === id);
     setCategoriaActual(categoria[0]);
+    router.push("/");
   };
 
-  const handleSetProducto = producto => {
-    setProducto(producto)
-  }
+  const handleSetProducto = (producto) => {
+    setProducto(producto);
+  };
 
   const handleChangeModal = () => {
-    setModal(!modal)
-  }
+    setModal(!modal);
+  };
 
-  const handleAgregarPedido = ({ categoriaId, imagen, ...producto }) => {
-    if (pedido.some(productoState => productoState.id === producto.id)) {
-      const pedidoActualizado = pedido.map(productoState => productoState.id === producto.id ? producto : productoState)
-      setPedido(pedidoActualizado)
+  const handleAgregarPedido = ({ categoriaId, ...producto }) => {
+    if (pedido.some((productoState) => productoState.id === producto.id)) {
+      const pedidoActualizado = pedido.map((productoState) =>
+        productoState.id === producto.id ? producto : productoState
+      );
+      setPedido(pedidoActualizado);
       toast.success("Carrito Actualizado");
-
     } else {
       setPedido([...pedido, producto]);
-      toast.success('Añadido al Carrito')
+      toast.success("Añadido al Carrito");
     }
 
-    setModal(false)
-  }
+    setModal(false);
+  };
 
-  const handleChangePaso = paso => {
-    setPaso(paso)
-  }
+  const handleEditarCantidades = (id) => {
+    const productoActualizar = pedido.filter((producto) => producto.id === id);
+    setProducto(productoActualizar[0]);
+    setModal(!modal);
+  };
+
+  const handleEliminarProducto = (id) => {
+    const pedidoActualizador = pedido.filter((producto) => producto.id !== id);
+    setPedido(pedidoActualizador);
+  };
+
+  const colocarOrden = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/api/ordenes", {
+        pedido,
+        nombre,
+        total,
+        fecha: Date.now().toString(),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    setCategoriaActual(categorias[0]);
+    setPedido([]);
+    setNombre("");
+    setTotal(0);
+
+    toast.success('Pedido Realizado Correctamente')
+
+    setTimeout(() => {
+      router.push('/')
+    }, 3000)
+  };
 
   return (
     <QuioscoContext.Provider
@@ -69,16 +114,18 @@ const QuioscoProvider = ({ children }) => {
         handleChangeModal,
         handleAgregarPedido,
         pedido,
-        paso,
-        handleChangePaso,
+        handleEditarCantidades,
+        handleEliminarProducto,
+        nombre,
+        setNombre,
+        colocarOrden,
+        total,
       }}
     >
       {children}
     </QuioscoContext.Provider>
   );
-}
+};
 
-export {
-  QuioscoProvider
-}
-export default QuioscoContext
+export { QuioscoProvider };
+export default QuioscoContext;
--- pages/resumen.js
+++ pages/resumen.js
@@ -1,10 +1,26 @@
 import Layout from "../layout/Layout"
+import useQuiosco from "../hooks/useQuiosco"
+import ResumenProducto from "../components/ResumenProducto"
 
 export default function Resumen() {
+
+  const {pedido} = useQuiosco()
+
   return (
     <Layout pagina={'Resumen'}>
       <h1 className="text-4xl font-black">Resumen</h1>
       <p className="text-2xl my-10">Resivasa tu Pedido</p>
+
+      {pedido.length === 0 ? (
+        <p className="text-center text-2xl">No hay productos en tu carrito</p>
+      ) : (
+          pedido.map(producto => (
+            <ResumenProducto
+              key={producto.id}
+              producto={producto}
+            />
+          ))
+      )}
     </Layout>
   )
 }
